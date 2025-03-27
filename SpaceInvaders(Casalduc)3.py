c2cc5dd5d5b86d36e27be067d8cfca3b94e35ba0
Kaitlyn2025 <KCasalduc709385@muhsdstudents.org>
2025-03-27 15:01:47 -0700

    Add files via upload

---
+++ SpaceInvaders(Casalduc)3.py
@@ -0,0 +1,106 @@
+import pygame
+from pygame.locals import *
+
+clock = pygame.time.Clock()
+fps = 60
+
+
+screen_width = 600
+screen_height = 800
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Space Invaders')
+
+
+red = (255, 0, 0)
+green = (0, 255, 0)
+
+
+bg = pygame.image.load("img/bg.png")
+
+def draw_bg():
+    screen.blit(bg, (0, 0))
+
+
+class Spaceship(pygame.sprite.Sprite):
+    def __init__(self, x, y, health):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("img/spaceship.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+        self.health_start = health
+        self.health_remaininng = health
+        self.last_shot = pygame.time.get_ticks()
+
+
+    def update(self):
+        speed = 8
+
+        cooldown = 500
+
+        key = pygame.key.get_pressed()
+        if key[pygame.K_LEFT] and self.rect.left > 0:
+            self.rect.x -= speed
+        if key[pygame.K_RIGHT] and self.rect.right < screen_width:
+            self.rect.x += speed
+
+        time_now = pygame.time.get_ticks()
+
+        if key[pygame.K_SPACE] and time_now - self.last_shot > cooldown:
+            bullet = Bullets(self.rect.centerx, self.rect.top)
+            bullet_group.add(bullet)
+            self.last_shot = time_now
+            
+
+        pygame.draw.rect(screen, red, (self.rect.x, (self.rect.bottom + 10), self.rect.width, 15))
+        if self.health_remaining > 0:
+            pygame.draw.rect(screen, green, (self.rect.x, (self.rect.bottom + 10), int(self.rect.width * (self.health_remaining / self.health_start)), 15))
+
+
+
+class Bullets(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("img/bullet.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+
+
+    def update(self):
+        self.rect.y -= 5
+        if self.rect.bottom < 0:
+            self.kill()
+
+spaceship_group = pygame.sprite.Group()
+bullet_group = pygame.sprite.Group()
+
+
+spaceship = Spaceship(int(screen_width / 2), screen_height - 100, 3)
+spaceship_group.add(spaceship)
+
+
+run = True
+while run:
+
+    clock.tick(fps)
+
+    draw_bg()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+
+    spaceship.update()
+
+
+    bullet_group.update()
+    
+
+    spaceship_group.draw(screen)
+    bullet_group.draw(screen)
+
+    pygame.display.update()
+
+pygame.quit()
+     
